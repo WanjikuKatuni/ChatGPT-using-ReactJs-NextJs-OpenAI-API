37148275911fbccf3197b5d16e29d69052a771dc
wanjiku <annritawanjiku77@gmail.com>
2023-03-29 11:10:04 +0300

    changed port to 3080

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const openai = new OpenAIApi(configuration);
 // simple express api which calls the function above
 
 const app = express()
-const port = 3000
+const port = 3080
 
 app.post('/', async (req, res) => {
     
