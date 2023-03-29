99cb26de7f990df5deeb5abe931346aee55fd35b
wanjiku <annritawanjiku77@gmail.com>
2023-03-29 11:08:58 +0300

    created express API routes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-
 const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+
 const configuration = new Configuration({
     organization: "org-9DVpsfYS5ybHlRqrTRyHW51q",
     apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-async function callApi(){
+
+
+// simple express api which calls the function above
+
+const app = express()
+const port = 3000
+
+app.post('/', async (req, res) => {
+    
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Say this is a test",
@@ -14,18 +23,9 @@ async function callApi(){
         temperature: 0,
       });
       console.log(response.data.choices[0].text)
-}
-
-callApi()
-
-// simple express api which calls the function above
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+      res.json({
+        data: response.data
+      })
 })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
