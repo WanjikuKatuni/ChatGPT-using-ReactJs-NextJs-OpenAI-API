62b1a6ec13d73847ee80c8bc7baaef1d196b99a7
wanjiku <annritawanjiku77@gmail.com>
2023-03-29 12:33:27 +0300

    creating frontend API request using fetch

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
+// add  body parse and cors to express
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const configuration = new Configuration({
     organization: "org-9DVpsfYS5ybHlRqrTRyHW51q",
-    apiKey: "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",
+    apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
@@ -12,19 +15,28 @@ const openai = new OpenAIApi(configuration);
 // simple express api which calls the function above
 
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
 const port = 3080
 
 app.post('/', async (req, res) => {
-    
+    const {message} = req.body
+    // console.log(message)
+
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
       });
-      console.log(response.data.choices[0].text)
+
+    //   console.log(response.data.choices[0].text)
       res.json({
-        data: response.data
+        message: response.data.choices[0].text
+        // data: message,
       })
 })
 app.listen(port, () => {
